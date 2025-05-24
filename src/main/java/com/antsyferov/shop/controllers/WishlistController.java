275ea2abad02b21c7e005b99e604a8cc99da9578
alekseiantsyferov <aleksei.antsyferov@gmail.com>
2025-05-24 16:22:52 +0300

    added whishlist

---
+++ src/main/java/com/antsyferov/shop/controllers/WishlistController.java
@@ -0,0 +1,12 @@
+package com.antsyferov.shop.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WishlistController {
+    @GetMapping("/wishlist")
+    public String wishlist() {
+        return "cozastore-master/wishlist";
+    }
+} 
